f9debb4d1edece2790faeb855d52ffc3830e3588
Ahmed Abdrabou <a.abdrabou110@icloud.com>
2022-12-08 23:59:14 +0200

    develop a design with no reponsive

---
+++ scripts/signup.js
@@ -0,0 +1,11 @@
+//select all elements
+const createAccountBtn = document.querySelector(".button__container button");
+const usernameInput = document.querySelector("#username");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#username");
+const confirmPasswordInput = document.querySelector("#confirm");
+
+// submit form
+createAccountBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+});
