b6f66776b84c5821d8a60352884c526ca2e96bcb
Ahmed Abdrabou <a.abdrabou110@icloud.com>
2022-12-09 18:19:45 +0200

    initialize the logic with javascript

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,11 +1,137 @@
 //select all elements
 const createAccountBtn = document.querySelector(".button__container button");
-const usernameInput = document.querySelector("#username");
-const emailInput = document.querySelector("#email");
-const passwordInput = document.querySelector("#username");
-const confirmPasswordInput = document.querySelector("#confirm");
+const usernameInput = document.querySelector("input#username");
+const emailInput = document.querySelector("input#email");
+const passwordInput = document.querySelector("input#password");
+const confirmPasswordInput = document.querySelector("input#confirmPassword");
+const form = document.querySelector("form");
 
-// submit form
+//! set Error Function
+const showError = (element, message) => {
+  const formControl = element.parentElement.parentElement;
+  const small = formControl.querySelector(".error");
+  small.textContent = message;
+};
+//! set success Function
+const success = (element) => {
+  const formControl = element.parentElement.parentElement;
+  const small = formControl.querySelector(".error");
+  small.textContent = "";
+};
+
+// ! validate username , username => Username must consist of 5 to 15 characters, only letters and numbers are allowed, with no numbers at the beginning or the end
+
+const validateUsername = (username) => {
+  const pattern = /^[a-z][a-z0-9]*([a-z]+){5,15}$/gi;
+  const valid = pattern.test(username);
+  if (valid) {
+    success(usernameInput);
+    return true;
+  } else if (username.trim() === "") {
+    showError(usernameInput, "Username mustn't leave blank");
+    return;
+  }
+  if (username.length < 5) {
+    showError(usernameInput, "Username must too short should between 5-15");
+    return;
+  } else if (username.length > 15) {
+    showError(usernameInput, "Username must too long should between 5-15");
+    return;
+  } else {
+    showError(
+      usernameInput,
+      "Username must consist of 5 to 15 characters, only letters and numbers are allowed, with no numbers at the beginning or the end"
+    );
+    return;
+  }
+};
+
+//! Validate Email
+const validateEmail = (email) => {
+  const regex = /^\w+@[a-z]+\.[a-z]{2,3}/;
+  const valid = regex.test(email);
+  if (valid) {
+    localStorage.setItem("email", email);
+    success(emailInput);
+    return true;
+  } else {
+    showError(emailInput, "Please , Enter a valid Email");
+    return;
+  }
+};
+
+//! validate password => password at least 8 character
+const validatePassword = (password) => {
+  if (password.length >= 8) {
+    success(passwordInput);
+    return true;
+  } else {
+    showError(passwordInput, "password must be at least 8 character");
+    return;
+  }
+};
+
+//! Validate confirm password
+
+const validateConfirmPassword = (password1, password2) => {
+  if (password2.length >= 8) {
+    if (password1 === password2) {
+      success(confirmPasswordInput);
+      return true;
+    } else {
+      showError(confirmPasswordInput, "The password does not match");
+    }
+  } else {
+    showError(confirmPasswordInput, "password should be at least 8 character");
+  }
+};
+
+const valiadeForm = () => {
+  const usernameValue = usernameInput.value;
+  const emailValue = emailInput.value;
+  const passwordValue = passwordInput.value;
+  const confirmPasswordValue = confirmPasswordInput.value;
+  if (
+    validateUsername(usernameValue) &&
+    validateEmail(emailValue) &&
+    validatePassword(passwordValue) &&
+    validateConfirmPassword(passwordValue, confirmPasswordValue)
+  ) {
+    return true;
+  }
+};
+
+//! submit form
 createAccountBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  const usernameValue = usernameInput.value;
+  const emailValue = emailInput.value;
+  const passwordValue = passwordInput.value;
+  const confirmPasswordValue = confirmPasswordInput.value;
+  if (
+    validateUsername(usernameValue) &&
+    validateEmail(emailValue) &&
+    validatePassword(passwordValue) &&
+    validateConfirmPassword(passwordValue, confirmPasswordValue)
+  ) {
+    fetch("https://goldblv.com/api/hiring/tasks/register", {
+      method: "POST",
+      body: JSON.stringify({
+        username: usernameValue,
+        email: emailValue,
+        password: passwordValue,
+        password_confirmation: confirmPasswordValue,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((data) => {
+        if (data.ok) {
+          window.location = "../success.html";
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+  // return false;
 });
